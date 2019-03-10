20c47614047852b90964392181576c8ab848c028
NYHolzer <nissan.holzer@gmail.com>
2019-03-10 11:24:44 -0400

    Setup Gem Files with "bundle gem amzscrapper"

---
+++ amzscrapper/lib/amzscrapper.rb
@@ -0,0 +1,6 @@
+require "amzscrapper/version"
+
+module Amzscrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ amzscrapper/lib/amzscrapper/version.rb
@@ -0,0 +1,3 @@
+module Amzscrapper
+  VERSION = "0.1.0"
+end
