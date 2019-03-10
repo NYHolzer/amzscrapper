78ca9f6980a070e543a2990d5a8acf7dc22ac9d1
NYHolzer <nissan.holzer@gmail.com>
2019-03-10 19:19:42 -0400

    some insight into scrapping amazon

--- amzscrapper/lib/amzscrapper/scrapper.rb
+++ amzscrapper/lib/amzscrapper/scrapper.rb
@@ -2,13 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-site = "https://www.amazon.com/s?k=lew+electric&ref=nb_sb_noss_2"
-html = open(site,  "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
+site = "https://www.amazon.com/s?k=ipad+pro&i=electronics&ref=nb_sb_noss_1"
+html = open(site, "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36" )
 
 doc = Nokogiri::HTML(html)
 
 #:asin
-#:title = doc.css("div.a-section.a-spacing-none.a-spacing-top-small").first.css("h5").text
+#:title = doc.css(".s-line-clamp-2 , .a-color-base.a-text-normal")[0].text
 #:price
 
 binding.pry
